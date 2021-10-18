7aae66d4a789ea6d40137bad7d3feab71f4851a0
pavelv <pavelv@D476>
2021-10-18 22:51:08 +0300

    Add project files.

---
+++ 3or5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3or5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 999; i++)
+            {
+                if (i%3 ==0 || i%5 == 0)
+                    sum = sum + i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ sumOfPrimes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace sumOfPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 5;
+            bool err ;
+            for (int i = 5; i <= 200000; i=i+2)
+            {
+                err = false;
+                for (int m = 2; m < i; m++)
+                {
+                    if (i % m == 0)
+                    {
+                        err = true;
+                        break;
+                    }
+                }
+                if (err == false)
+                    sum = sum + i;
+            }
+            Console.WriteLine("Сумма {0}", sum);
+        }
+    }
+}
