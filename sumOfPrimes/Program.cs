9d5456745bdab0da690b6034c552f6819e0ffcc5
greenBob24 <greenBob24@yandex.ru>
2021-10-20 16:38:47 +0300

    upgrade 2000000

--- sumOfPrimes/Program.cs
+++ sumOfPrimes/Program.cs
@@ -1,31 +1,51 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace sumOfPrimes
 {
     class Program
     {
         static void Main(string[] args)
-        {   // сумма простых чисел до 20000000
+        {   // Сумма простых чисел меньше 10 равна 2 + 3 + 5 + 7 = 17.Найдите сумму всех простых чисел меньше двух миллионов.
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            long sum = 5;
-            bool err ;
-            for (int i = 5; i <= 2000000; i=i+2)
+            List<long> numbers = new List<long>();
+            long sum = 17;
+            bool err = false;
+            for (int i = 11; i < 2000000; i = i + 10)
+            {
+                numbers.Add(i);
+            }
+            for (int i = 13; i < 2000000; i = i + 10)
+            {
+                numbers.Add(i);
+            }
+            for (int i = 17; i < 2000000; i = i + 10)
+            {
+                numbers.Add(i);
+            }
+            for (int i = 19; i < 2000000; i = i + 10)
+            {
+                numbers.Add(i);
+            }
+
+            foreach (int elem in numbers)
             {
                 err = false;
-                for (int m = 3; m < i; m=m+2)
+                for (int m = 3; m <= Convert.ToInt32(Math.Sqrt(elem)); m = m + 2)
                 {
-                    if (i % m == 0)
-                    {   
+                    if (elem % m == 0)
+                    {
                         err = true;
                         break;
                     }
                 }
                 if (err == false)
-                    sum = sum + i;
-
+                {
+                    sum = sum + elem;
+                }
             }
             Console.WriteLine(sum);
             stopWatch.Stop();
@@ -33,8 +53,8 @@ namespace sumOfPrimes
 
             // Format and display the TimeSpan value.
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
+ts.Hours, ts.Minutes, ts.Seconds,
+ts.Milliseconds / 10);
             Console.WriteLine("RunTime " + elapsedTime);
         }
     }
